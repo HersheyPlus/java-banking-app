b4ff77cfc038496ef8a9364c2fa6bf64aa13c612
Melbromss <melbromss@gmail.com>
2024-05-31 15:28:12 +0700

    edit main

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -7,14 +7,5 @@ import repository.FileRepo;
 
 public class Main {
     public static void main(String[] args) {
-        AccountRepo accService = new AccountRepo();
-        FileRepo fileService = new FileRepo();
-        accService.addAccount("Bomb","432lfsdfs",500);
-        accService.addAccount("Best","vsmklvmkkp",1500);
-        System.out.println(accService.getAllAccounts());
-        System.out.println("--- Write file ---");
-        fileService.WriteFile(accService.getAllAccounts());
-        System.out.println("--- Read file ---");
-        fileService.ReadFile();
     }
 }
